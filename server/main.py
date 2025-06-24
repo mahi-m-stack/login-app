4fab80d30637108496dc4fe3d27364dff3a075d2
mahi-m-stack <mahi.m.stack@gmail.com>
2025-06-24 19:39:08 +0530

    Main remote main branch

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://login-app-sage-rho.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
